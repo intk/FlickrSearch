8fa7e355cec321ed2c46b5da1ee29bed414982cc
André Gonçalves <andreslb1@gmail.com>
2016-01-20 16:20:24 +0100

    Initial commit

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Andre Goncalves on 20/01/16.
+//  Copyright © 2016 INTK. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+FlickrSearch iOS app build by following the tutorial: http://www.raywenderlich.com/78550/beginning-ios-collection-views-swift-part-1
+
